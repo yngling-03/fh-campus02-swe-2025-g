df3b94be0d012310b86ac1b2861fca361c0b807c
janikalmercode <janikalmerbusiness@gmail.com>
2025-10-04 12:19:58 +0200

    delete !

--- src/main/java/at/campus02/swe/parser/Parser.java
+++ src/main/java/at/campus02/swe/parser/Parser.java
@@ -79,6 +79,6 @@ public class Parser {
         else if ("-".equals(value))
             return Operation.sub;
 
-        throw new CalculatorException("Unsupported Operation!");
+        throw new CalculatorException("Unsupported Operation");
     }
 }
